bfdad210d7b15a8f30ebd2df6fbc6f741a803864
daencer <33512814+daencer@users.noreply.github.com>
2017-11-09 11:15:32 +0100

    test

---
+++ dice_game_main.py
@@ -0,0 +1,32 @@
+"""Docstring: A very short sentence explaining the function. < 79 characters.
+
+additional informaion if required
+and more infos
+
+"""
+
+from math import sqrt, log, e  # an example
+
+# from numpy import array  #  an aother example
+# from meinmodul import test  #  example for your own module
+
+__author__ = "123456: John Cleese, 654321: Terry Gilliam"  # your data
+__copyright__ = "Copyright 2015/2016 – EPR-Goethe-Uni"
+__credits__ = "If you would like to thank somebody \
+i.e. an other student for his code"
+__email__ = "your email address"
+
+
+def yourroutine_1():
+    """Docstring: Every subroutine and function has a docstring.
+
+    """
+    pass  # do nothing
+
+
+def yourfunction_2():
+    """Docstring
+
+    """
+
+    print("yourfunction was called--not yet written or incomplete")
